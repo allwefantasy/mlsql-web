44beefe2823b9073dd499bf0d1f0388b4b286497
WilliamZhu <allwefantasy@gmail.com>
2018-07-02 14:10:06 +0800

    update

--- test/unit/specs/Query.spec.js
+++ test/unit/specs/Query.spec.js
@@ -1,22 +1,24 @@
 import Vue from 'vue'
-import Query from '@/components/Query'
 import sinon from 'sinon'
 import {getVM, initVueResource} from "./helper/VueHelper"
+import Query from '../../../src/components/Query'
 
-var sandbox = sinon.createSandbox()
 
 describe('Query', () => {
-  let server
 
   //in Query component we need vue-resource
   initVueResource()
 
+  let sandbox
+  let server
+
   beforeEach(() => {
+    sandbox = sinon.createSandbox()
     server = sinon.fakeServer.create()
   })
   afterEach(() => {
-    sandbox.restore()
     server.restore()
+    sandbox.restore()
   })
 
   it('#submitRunSQL should work fine', () => {
@@ -28,20 +30,18 @@ describe('Query', () => {
       "jobName": vm.jobName
     }
 
-    const expectedBody = {
-      data: [{
-        a: 1, b: "jack"
-      }]
-    }
+    const expectedBody = [{
+      a: 1, b: "jack"
+    }]
 
-    server.respondWith('POST', "/run/script", (xhr) => {
+    server.respondWith('POST', vm.resource.job_url, (xhr) => {
       xhr.respond(200, {'Content-Type': 'application/json'}, JSON.stringify(expectedBody))
     })
 
-    server.autoRespond = true
-
+    //server.autoRespond = true
     vm.submitRunSQL({})
-
-    //expect(vm.$refs.show_detail_table).toEqual('select 1 as a,\'jack\' as b as bbc;')
+    server.respond()
+    expect(vm.result.tableData).toEqual(expectedBody)
+    //expect(vm.$el.querySelector("#show_result").textContent).toEqual('select 1 as a,\'jack\' as b as bbc;')
   })
 })
